a4e2a93ec84e63e2cd8cfe340a56ab31de2dc979
akhlaghimhd <77932814+akhlaghimhd@users.noreply.github.com>
2026-09-15 21:31:52 +0330

    FE-P1: export Sprint 3/4 identity pages and services

--- src/modules/identity/index.ts
+++ src/modules/identity/index.ts
@@ -7,9 +7,15 @@ export { ProfileMePage } from "./pages/profile-me";
 export { MembersListPage } from "./pages/members-list";
 export { MemberDetailPage } from "./pages/member-detail";
 export { MemberCreatePage } from "./pages/member-create";
+export { RolesListPage } from "./pages/roles-list";
+export { RoleDetailPage } from "./pages/role-detail";
+export { PermissionsListPage } from "./pages/permissions-list";
+export { ScopesListPage } from "./pages/scopes-list";
 export { profileService } from "./services/profile-service";
 export { tenantUserService } from "./services/tenant-user-service";
 export { roleService } from "./services/role-service";
+export { permissionService } from "./services/permission-service";
+export { scopeService } from "./services/scope-service";
 export { membershipHistoryService } from "./services/membership-history-service";
 export { identityPaths } from "./services/paths";
 export {
@@ -31,6 +37,32 @@ export {
   useMembershipHistory,
   membershipHistoryQueryKey,
 } from "./hooks/use-membership-history";
+export {
+  useRoles,
+  useRole,
+  useCreateRole,
+  useUpdateRole,
+  useSoftDeleteRole,
+  useAssignRoleToUser,
+  useAssignPermissionsToRole,
+  rolesQueryKey,
+} from "./hooks/use-roles";
+export {
+  usePermissions,
+  useCreatePermission,
+  useUpdatePermission,
+  useSoftDeletePermission,
+  permissionsQueryKey,
+} from "./hooks/use-permissions";
+export {
+  useScopes,
+  useCreateScope,
+  useUpdateScope,
+  useSoftDeleteScope,
+  useUserScopes,
+  useAssignScopesToUser,
+  scopesQueryKey,
+} from "./hooks/use-scopes";
 export {
   IdentityPermissions,
   type UserProfileDto,
